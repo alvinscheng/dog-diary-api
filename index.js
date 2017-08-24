067d115f71b0a192fc48d75bc4c1c678ba760004
Alvin Cheng <ascheng21@gmail.com>
2017-08-23 21:47:53 -0700

    Added GET route to get dogs from db. #1

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
-const multer = require('multer')
 const knex = require('knex')({
   dialect: 'pg',
   connection: process.env.DATABASE_URL
 })
-const upload = multer({ dest: 'uploads/' })
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.post('/dogs', upload.single('picture'), (req, res) => {
+app.post('/dogs', (req, res) => {
   console.log(req.body)
   knex('dogs').insert(req.body).then(() => res.sendStatus(201))
 })
 
-// app.post('/users', upload.single('picture'), (req, res) => {
-//   const user = snakecaseKeys(req.body)
-//   if (req.file) {
-//     user.picture = req.file.filename
-//   }
-//   users
-//     .create(user)
-//     .then(data => {
-//       res.json(data)
-//       res.sendStatus(201)
-//     })
-// })
+app.get('/dogs', (req, res) => {
+  knex('dogs').orderBy('id', 'desc')
+    .then(data => {
+      res.json(data)
+    })
+})
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log('Listening on ' + PORT))
