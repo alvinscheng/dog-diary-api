9e07af9f52cad5aaaac31b0baced8080fc4314d6
Alvin Cheng <ascheng21@gmail.com>
2017-08-23 19:13:24 -0700

    Added POST route to add dogs to a db. #1

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
+const multer = require('multer')
 const knex = require('knex')({
   dialect: 'pg',
   connection: process.env.DATABASE_URL
 })
+const upload = multer({ dest: 'uploads/' })
 
 const app = express()
 
 app.use(bodyParser.json())
+
+// app.post('/dogs', upload.single('picture'), (req, res) => {
+//
+// })
+
+// app.post('/users', upload.single('picture'), (req, res) => {
+//   const user = snakecaseKeys(req.body)
+//   if (req.file) {
+//     user.picture = req.file.filename
+//   }
+//   users
+//     .create(user)
+//     .then(data => {
+//       res.json(data)
+//       res.sendStatus(201)
+//     })
+// })
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log('Listening on ' + PORT))
