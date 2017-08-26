1ef999c1d6c9bb27f79dabc61b92dce71f2adab1
Alvin Cheng <ascheng21@gmail.com>
2017-08-26 11:46:28 -0700

    Added CORS as dependency to serve image upload files from server. #1

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
-var multer  = require('multer')
-var upload = multer({ dest: 'uploads/' })
+const path = require('path')
+const multer  = require('multer')
+const upload = multer({ dest: 'public/uploads/' })
+const cors = require('cors')
 const knex = require('knex')({
   dialect: 'pg',
   connection: process.env.DATABASE_URL
@@ -10,7 +12,9 @@ const knex = require('knex')({
 
 const app = express()
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json())
+app.use(cors())
 
 app.post('/dogs', upload.single('profile_picture'), (req, res) => {
   const { name, age } = req.body
@@ -25,9 +29,18 @@ app.post('/dogs', upload.single('profile_picture'), (req, res) => {
 app.get('/dogs', (req, res) => {
   knex('dogs').orderBy('id', 'desc')
     .then(data => {
+      console.log('/uploads/' + data[0].profile_picture)
       res.json(data)
     })
 })
+
+// app.get('/dog/:name', (req, res) => {
+//   const { name } = req.params
+//   knex('dogs').where('name', name).limit(1)
+//     .then(data => {
+//       res.sendFile(path.join(__dirname, '/uploads', data[0].profile_picture))
+//     })
+// })
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log('Listening on ' + PORT))
